cf5d228379a079eba1ddb3049da70fb65657eba3
TongQuangThanh <tongquangthanh94@gmail.com>
2022-09-03 20:51:15 +0700

    rerun

--- dist/mongoose/movie.js
+++ dist/mongoose/movie.js
@@ -32,7 +32,7 @@ const movieSchema = new mongoose_1.default.Schema({
     category: [{ name: String }],
     country: [{ name: String }],
 });
-movieSchema.index({ name: 'text', slug: 'text' });
+movieSchema.index({ origin_name: 'text', slug: 'text' });
 const model = mongoose_1.default.model('Movie', movieSchema);
 model.createIndexes();
 exports.MovieSchema = model;
